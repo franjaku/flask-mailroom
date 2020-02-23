11b1373939a1def16c5313eb24d0128e3ad54f7b
franjaku <kulyckyf@umich.edu>
2020-02-22 16:40:15 -0800

    baseline

--- main.py
+++ main.py
@@ -7,17 +7,28 @@ from model import Donation
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def home():
     return redirect(url_for('all'))
+
 
 @app.route('/donations/')
 def all():
     donations = Donation.select()
     return render_template('donations.jinja2', donations=donations)
-    
+
+
+@app.route('/create', methods=['GET','POST'])
+def create_donation():
+    if request.method == 'POST':
+        Donation.create(value=request.form['donation'], donor=request.form['name'])
+        redirect(url_for('donations'))
+    else:
+        return render_template('create.jinja2')
+
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 6738))
-    app.run(host='0.0.0.0', port=port)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='127.0.0.1', port=port)
 
